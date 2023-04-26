981777356fdff6e1edb431dbf3df52209453573a
liguangwei007 <liguwe@qq.com>
2023-04-26 14:17:45 +0800

    update

--- baidu.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?7f418d7e0082dad3f4931de09ee4ae02";
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(hm, s);
-})();
