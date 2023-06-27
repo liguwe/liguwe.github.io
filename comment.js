e5d0f1d15b711dc3b44e9accfdd0cbc9fbd08167
liguangwei <liguwe@qq.com>
2023-06-27 13:23:04 +0800

    update

--- comment.js
+++ comment.js
@@ -1,4 +1,6 @@
-(function() {
+function addComment() {
+    const comment = document.querySelector('#liguwe-comment');
+    if(comment.innerHTML) return;
     const script = document.createElement('script');
     script.src = 'https://utteranc.es/client.js';
     script.async = true;
@@ -7,5 +9,28 @@
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('label', 'Comment');
     script.setAttribute('theme', 'github-dark');
-    document.querySelector('#liguwe-comment').appendChild(script);
-})();
+    comment.appendChild(script);
+}
+
+addComment();
+const bindEventListener = function (type) {
+    const historyEvent = history[type];
+    return function () {
+        const newEvent = historyEvent.apply(this, arguments);
+        const e = new Event(type);
+        e.arguments = arguments;
+        window.dispatchEvent(e);
+        return newEvent;
+    };
+};
+history.pushState = bindEventListener('pushState');
+history.replaceState = bindEventListener('replaceState');
+window.addEventListener('replaceState', function (e) {
+    addComment();
+});
+window.addEventListener('pushState', function (e) {
+    addComment();
+});
+window.addEventListener("popstate", function (event) {
+    addComment();
+})
