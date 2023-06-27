d5a076dee4c3e40f6656f64a7bf9e3de8140b90f
liguangwei <liguwe@qq.com>
2023-06-27 10:04:07 +0800

    update

--- comment.js
+++ comment.js
@@ -1,9 +1,11 @@
-const script = document.createElement('script');
-script.src = 'https://utteranc.es/client.js';
-script.async = true;
-script.crossOrigin = 'anonymous';
-script.setAttribute('repo', 'liguwe/liguwe.github.io');
-script.setAttribute('issue-term', 'pathname');
-script.setAttribute('label', 'Comment');
-script.setAttribute('theme', 'github-dark');
-document.querySelector('#liguwe-comment').appendChild(script);
+(function() {
+    const script = document.createElement('script');
+    script.src = 'https://utteranc.es/client.js';
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.setAttribute('repo', 'liguwe/liguwe.github.io');
+    script.setAttribute('issue-term', 'pathname');
+    script.setAttribute('label', 'Comment');
+    script.setAttribute('theme', 'github-dark');
+    document.querySelector('#liguwe-comment').appendChild(script);
+})();
