762561e31ca82fddfe2932b75d6c89cbf29d01ac
Guangwei Li <liguwe@qq.com>
2024-11-18 12:29:01 +0800

    feat: deploy at 2024-11-18 12:29

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# [832 之书](https://liguwe.site)
 
-> 个人知识库合计
+# README.md
+
+个人知识库合计，更访问 [https://liguwe.site](https://liguwe.site)
--- codes/2024.11/1.js
+++ codes/2024.11/1.js
@@ -1,20 +1,38 @@
-// 递归解法
-var deleteDuplicatesRecursive = function (head) {
-  // 定义：输入一条单链表头结点，返回去重之后的单链表头结点
-  // base case
-  if (head === null || head.next === null) {
-    return head;
+var nthUglyNumber = function (n) {
+  // 可以理解为三个指向有序链表头结点的指针
+  let p2 = 1,
+    p3 = 1,
+    p5 = 1;
+  // 可以理解为三个有序链表的头节点的值
+  let product2 = 1,
+    product3 = 1,
+    product5 = 1;
+  // 可以理解为最终合并的有序链表（结果链表）
+  let ugly = new Array(n + 1);
+  // 可以理解为结果链表上的指针
+  let p = 1;
+
+  // 开始合并三个有序链表
+  while (p <= n) {
+    // 取三个链表的最小结点
+    let min = Math.min(Math.min(product2, product3), product5);
+    // 接到结果链表上
+    ugly[p] = min;
+    p++;
+    // 前进对应有序链表上的指针
+    if (min == product2) {
+      product2 = 2 * ugly[p2];
+      p2++;
+    }
+    if (min == product3) {
+      product3 = 3 * ugly[p3];
+      p3++;
+    }
+    if (min == product5) {
+      product5 = 5 * ugly[p5];
+      p5++;
+    }
   }
-  if (head.val !== head.next.val) {
-    // 如果头结点和身后节点的值不同，则对之后的链表去重即可
-    head.next = deleteDuplicatesRecursive(head.next);
-    return head;
-  }
-  // 如果如果头结点和身后节点的值相同，则说明从 head 开始存在若干重复节点
-  // 越过重复节点，找到 head 之后那个不重复的节点
-  while (head.next !== null && head.val === head.next.val) {
-    head = head.next;
-  }
-  // 直接返回那个不重复节点开头的链表的去重结果，就把重复节点删掉了
-  return deleteDuplicatesRecursive(head.next);
+  // 返回第 n 个丑数
+  return ugly[n];
 };
