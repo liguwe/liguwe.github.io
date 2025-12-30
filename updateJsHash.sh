2426c17eb726936382a85a1f092db765f71b0a2c
Guangwei Li <liguwe@qq.com>
2025-12-30 13:39:02 +0800

    feat: 更新脚本

--- updateJsHash.sh
+++ updateJsHash.sh
@@ -10,11 +10,11 @@ INDEX_FILE="theme/index.hbs"
 cp "$INDEX_FILE" "$INDEX_FILE.bak"
 
 # 使用 sed 替换 toc.js 的引用，添加时间戳参数
-# 匹配 <script src="{{ path_to_root }}toc.js"></script> 或 <script src="{{ path_to_root }}toc.js?v=任意数字"></script>
-sed -i '' "s|<script src=\"{{ path_to_root }}toc.js\(?\[^\"]*\)\?\"></script>|<script src=\"{{ path_to_root }}toc.js?v=$TIMESTAMP\"></script>|g" "$INDEX_FILE"
+# 匹配 <script src="{{ path_to_root }}toc.js?v=任意内容"></script>
+# 替换为 <script src="{{ path_to_root }}toc.js?v=新时间戳"></script>
+sed -i '' "s|toc\.js?v=[0-9]*|toc.js?v=$TIMESTAMP|g" "$INDEX_FILE"
 
 echo "✅ 已更新 toc.js 的时间戳为: $TIMESTAMP"
 
 # 删除备份文件
 rm "$INDEX_FILE.bak"
-
